afae77e804f7d3be2118dc6dc6055ed2e9c054e0
Adelita Martinez <amartinez1013@cnm.edu>
2024-04-11 13:23:39 -0600

    Started react app

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+  <>
+    <div className="wrap">
+      <div className="alpaca">
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default App
